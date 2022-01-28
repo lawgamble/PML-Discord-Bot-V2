e5ae3e90d044823b2c28fc467b9d5caf0aa27b72
Lawrence Gamble <lsgrecording@gmail.com>
2022-01-27 21:48:26 -0600

    added cronjob for backup alias

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ const pickupGame = require("./alias-functions/pickups/pickupRefactor.js");
 const { clearVotesData, checkVoteCount, voter, clearUserVotes }= require("./alias-functions/pickups/voter.js");
 const hf  = require("./helperFunctions")
 const phf = require("./alias-functions/pickups/pickupHelperFunctions")
+var CronJob = require('cron').CronJob;
 
 
 
@@ -42,6 +43,9 @@ const client = new Client({
 
 // When bot logs in 
 client.on("ready", () => {
+    const data =  hf.readAliasData(filePath);
+    hf.writeAliasesData("backup.json", data);
+
     hf.readAliasFile(filePath, (error, data) => {
         if (error) {
           console.log(error);
@@ -52,6 +56,12 @@ client.on("ready", () => {
 
     });
     console.log("We Are LIVE!");
+
+    //create a cron job that runs every hour to copy aliases.js file to backup.js file
+    new CronJob('0 0 * * *', function() {
+       const data =  hf.readAliasData(filePath)
+       hf.writeAliasesData("backup.json", data);
+    }).start();
 });
 
 
